525557d94d24cb9b6cc3891348d2ccd9d5336528
barasapeter <barasapeter52@gmail.com>
2026-02-04 16:36:03 +0300

    Index static:ok

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from api.v1.api import api_router
 from core.config import settings
 from web.home import router as home_router
+from fastapi.staticfiles import StaticFiles
 
-app = FastAPI(title=settings.PROJECT_NAME)
+app = FastAPI(title=settings.PROJECT_NAME, docs_url=None, redoc_url=None, openapi_url=None)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 app.include_router(home_router, prefix="")
